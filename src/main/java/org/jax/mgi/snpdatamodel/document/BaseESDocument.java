4b8aa5af36cc02f0b63926c0a871c0da3e2063bc
Hongping Liang <hongping.liang@jax.org>
2025-09-23 11:53:38 -0400

    sprt-53: GXD Migration to ElasticSearch: FeWi Setup

--- src/main/java/org/jax/mgi/snpdatamodel/document/BaseESDocument.java
+++ src/main/java/org/jax/mgi/snpdatamodel/document/BaseESDocument.java
@@ -2,10 +2,18 @@ package org.jax.mgi.snpdatamodel.document;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BaseESDocument implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6501301092865333849L;
 	
 	private String consensussnp_accid;
 	
